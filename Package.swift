df23abb60fd68c545866066601c96c4ecc047669
Dmitry <dm1trym2130@gmail.com>
2022-08-09 16:59:40 +0300

    14.5

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CUIExpandableButton",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v14.5)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
